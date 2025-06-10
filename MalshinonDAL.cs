dbbed790f1f2fb0cb2e9670e10031e6b6e7c6920
matanya alkobi <matanor123456@gmail.com>
2025-06-10 12:44:45 +0300

    Adding a ChangeTypeByNameSearch method in the malshinon class that changes the person's type based on a name search

--- MalshinonDAL.cs
+++ MalshinonDAL.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Google.Protobuf.Compiler;
 using MySql.Data.MySqlClient;
 
@@ -199,6 +200,40 @@ namespace MalshinonProject
                 Console.WriteLine("General Error: " + ex.Message);
                 return "";
             }
+        }
+
+
+        //  A method that changes the person's type based on a name search
+        public static void ChangeTypeByNameSearch(string FirstName, string Type)
+        {
+            string Query = "UPDATE people " +
+                "SET Type = @Type " +
+                "WHERE FirstName = @FN;";
+
+            string connstring = "Server=localhost; database=Malshinon; UID=root; password=";
+            try
+            {
+                using (var connection = new MySqlConnection(connstring))
+                {
+                    connection.Open();
+                    using (var cmd = new MySqlCommand(Query, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@FN", FirstName);
+                        cmd.Parameters.AddWithValue("@Type", Type);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                Console.WriteLine("The type has been changed successfully!");
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("MySQL Error: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("General Error: " + ex.Message);
+            }
+
         }
 
     }
