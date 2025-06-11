53cc93c1ca5974a8a50c3f430308e6686d08bfe9
matanya alkobi <matanor123456@gmail.com>
2025-06-11 15:05:51 +0300

    Splitting departments by tables

--- DAL/PepoleTable/InsertPepoleDAL.cs
+++ DAL/PepoleTable/InsertPepoleDAL.cs
@@ -7,7 +7,7 @@ using MySql.Data.MySqlClient;
 
 namespace MalshinonProject
 {
-    internal class InsertMalshinonDAL
+    internal class InsertPepoleDAL
     {
         //  Method for adding a person to the people table
         public static void AddPerson(Person person)
@@ -47,38 +47,6 @@ namespace MalshinonProject
 
         }
 
-        // Method that adds a report to the IntelReport table
-        public static void AddingAReportToAIntelTable(int ReporterID, int TargetID, string Report)
-        {
-            string connectionString = "Server=localhost; database=Malshinon; UID=root; password=";
-            try
-            {
 
-
-                using (var connection = new MySqlConnection(connectionString))
-                {
-                    connection.Open();
-                    string query = "INSERT INTO intelreports (ReporterID, TargetID, Text) VALUES (@ReporterID,@TargetID, @Report)";
-                    using (var cmd = new MySqlCommand(query, connection))
-                    {
-
-                        cmd.Parameters.AddWithValue("@ReporterID", ReporterID);
-                        cmd.Parameters.AddWithValue("@TargetID", TargetID);
-                        cmd.Parameters.AddWithValue("@Report", Report);
-                        int Execution = cmd.ExecuteNonQuery();  // צריך  לטפל
-                    }
-                }
-                Console.WriteLine("Report added successfully");
-
-            }
-            catch (MySqlException ex)
-            {
-                Console.WriteLine("MySQL Error: " + ex.Message);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("General Error: " + ex.Message);
-            }
-        }
     }
 }
